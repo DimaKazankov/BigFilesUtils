6ea7cc76ccf053349d4a68f28c2c86e7cc791e55
Dim.Ka <dima.kazankov@gmail.com>
2024-09-27 16:29:59 +0200

    Added simple FileGenerator

--- src/BigFilesUtils/Program.cs
+++ src/BigFilesUtils/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using BigFilesUtils;
+
+if (args.Length != 2)
+{
+    Console.WriteLine("Usage: FileGenerator <file_path> <file_size_in_bytes>");
+    return;
+}
+
+var filePath = args[0];
+if (!long.TryParse(args[1], out var fileSizeInBytes))
+{
+    Console.WriteLine("Invalid file size.");
+    return;
+}
+
+new FileGenerator().GenerateFile(filePath, fileSizeInBytes);
+Console.WriteLine("File generated successfully.");
