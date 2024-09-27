9c337e5c8584e606e5d79c52f482e3cff252aa5c
Dim.Ka <dima.kazankov@gmail.com>
2024-09-27 16:29:59 +0200

    Added simple FileGenerator

---
+++ src/BigFilesUtils/FileGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace BigFilesUtils;
+
+public class FileGenerator
+{
+    private static readonly string[] SampleStrings = {
+        "Apple", "Banana is yellow", "Cherry is the best", "Something something something"
+    };
+
+    public void GenerateFile(string filePath, long fileSizeInBytes)
+    {
+        var random = new Random();
+        using var writer = new StreamWriter(filePath, false, Encoding.UTF8, 65536);
+        long currentSize = 0;
+        while (currentSize < fileSizeInBytes)
+        {
+            var number = random.Next(1, 1000000);
+            var str = SampleStrings[random.Next(SampleStrings.Length)];
+            var line = $"{number}. {str}";
+
+            writer.WriteLine(line);
+            currentSize += Encoding.UTF8.GetByteCount(line + Environment.NewLine);
+        }
+    }
+}
