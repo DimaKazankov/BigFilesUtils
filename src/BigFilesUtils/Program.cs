0b6ec51a4fcf4352ec83b36e6c4b1144232c2cba
Dim.Ka <dima.kazankov@gmail.com>
2024-09-27 21:38:13 +0200

    Cleaning files on pipeline. Better file size naming

--- src/BigFilesUtils/Program.cs
+++ src/BigFilesUtils/Program.cs
@@ -13,19 +13,63 @@ BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
 public class FileGeneratorBenchmark
 {
     private FileGenerator? _fileGenerator;
-    
-    [Params(1024, 1024 * 1024, 1024 * 1024 * 1024)]
+    private string? _fileName;
+
+    [Params(100 * 1024, 100 * 1024 * 1024, 100 * 1024L * 1024L * 1024L)]
     public long FileSizeInBytes { get; set; }
 
     [GlobalSetup]
     public void Setup()
     {
         _fileGenerator = new FileGenerator();
+        var sizeLabel = GetSizeLabel(FileSizeInBytes);
+        _fileName = $"{sizeLabel}_file_generator_benchmark.txt";
     }
 
     [Benchmark]
-    public void GenerateFile()
+    public void GenerateFile() => _fileGenerator!.GenerateFile(_fileName!, FileSizeInBytes);
+
+    [GlobalCleanup]
+    public void Cleanup()
     {
-        _fileGenerator!.GenerateFile($"{FileSizeInBytes}_bytes_file_generator_benchmark.txt", FileSizeInBytes);
+        if (_fileName == null || !File.Exists(_fileName))
+            return;
+        try
+        {
+            File.Delete(_fileName);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error deleting file {_fileName}: {ex.Message}");
+        }
+    }
+
+    private static string GetSizeLabel(long bytes)
+    {
+        const long gb = 1024L * 1024L * 1024L;
+        const long mb = 1024L * 1024L;
+        const long kb = 1024L;
+
+        switch (bytes)
+        {
+            case >= gb:
+            {
+                var sizeInGb = (double)bytes / gb;
+                return $"{sizeInGb:F2}GB";
+            }
+            case >= mb:
+            {
+                var sizeInMb = (double)bytes / mb;
+                return $"{sizeInMb:F2}MB";
+            }
+            case >= kb:
+            {
+                var sizeInKb = (double)bytes / kb;
+                return $"{sizeInKb:F2}KB";
+            }
+            default:
+                return $"{bytes}B";
+        }
     }
 }
+
