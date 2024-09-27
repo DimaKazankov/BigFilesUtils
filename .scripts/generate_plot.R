7a773b64eb7358451bf3dd1c245076bf4b8a3a95
Dim.Ka <dima.kazankov@gmail.com>
2024-09-27 20:21:15 +0200

    Improvments

--- .scripts/generate_plot.R
+++ .scripts/generate_plot.R
@@ -18,8 +18,12 @@ data$Q1 <- clean_ns(data$Q1)
 data$Q3 <- clean_ns(data$Q3)
 data$Median <- clean_ns(data$Median)
 
+# Ensure 'FileSizeInBytes' is treated as a factor (categorical variable)
+data$FileSizeInBytes <- as.factor(data$FileSizeInBytes)
+
 # Select the necessary columns for plotting
 metrics <- data.frame(
+  FileSizeInBytes = data$FileSizeInBytes,  # Include FileSizeInBytes for grouping
   Method = data$Method,
   Mean = data$Mean,
   StdDev = data$StdDev,
@@ -30,16 +34,17 @@ metrics <- data.frame(
 )
 
 # Melt the data for ggplot (long format for multiple metrics)
-metrics_melted <- melt(metrics, id.vars = 'Method', 
+metrics_melted <- melt(metrics, id.vars = c('Method', 'FileSizeInBytes'), 
                        variable.name = 'Metric', value.name = 'Value')
 
 # Ensure the 'Value' column is numeric
 metrics_melted$Value <- as.numeric(metrics_melted$Value)
 
-# Create a grouped bar chart showing different metrics for each method
+# Create a grouped bar chart showing different metrics for each method and file size
 p <- ggplot(metrics_melted, aes(x = Method, y = Value, fill = Metric)) +
   geom_bar(stat="identity", position = "dodge") +
-  labs(title="Fibonacci Benchmark Results", x="Method", y="Time (ns)", fill="Metric") +
+  facet_wrap(~ FileSizeInBytes) +  # Create separate plots for each FileSizeInBytes
+  labs(title="File Generator Benchmark Results", x="Method", y="Time (ns)", fill="Metric") +
   theme_minimal(base_size = 15) +
   scale_fill_brewer(palette="Set2") +  # Using a color palette from RColorBrewer
   theme(
