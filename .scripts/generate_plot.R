3b266099bf99c256014b574ab9a309f43963fff5
Dim.Ka <dima.kazankov@gmail.com>
2024-09-27 17:12:28 +0200

    Fix Beanchmark as plots color

--- .scripts/generate_plot.R
+++ .scripts/generate_plot.R
@@ -36,9 +36,6 @@ metrics_melted <- melt(metrics, id.vars = 'Method',
 # Ensure the 'Value' column is numeric
 metrics_melted$Value <- as.numeric(metrics_melted$Value)
 
-# Print the first few rows for debugging
-print(head(metrics_melted))
-
 # Create a grouped bar chart showing different metrics for each method
 p <- ggplot(metrics_melted, aes(x = Method, y = Value, fill = Metric)) +
   geom_bar(stat="identity", position = "dodge") +
@@ -46,13 +43,17 @@ p <- ggplot(metrics_melted, aes(x = Method, y = Value, fill = Metric)) +
   theme_minimal(base_size = 15) +
   scale_fill_brewer(palette="Set2") +  # Using a color palette from RColorBrewer
   theme(
-    axis.text.x = element_text(angle=45, hjust=1),  # Rotate x-axis labels for readability
-    plot.title = element_text(size=20, face="bold", hjust=0.5),
-    axis.title.x = element_text(face="bold", size=15),
-    axis.title.y = element_text(face="bold", size=15),
+    plot.title = element_text(size=20, face="bold", hjust=0.5, color="white"),
+    axis.title.x = element_text(face="bold", size=15, color="white"),
+    axis.title.y = element_text(face="bold", size=15, color="white"),
+    axis.text.x = element_text(angle=45, hjust=1, color="white"),
+    axis.text.y = element_text(color="white"),
+    legend.title = element_text(color="white"),
+    legend.text = element_text(color="white"),
+    panel.grid = element_line(color = "gray50"),
     legend.position = "bottom"
   ) +
-  scale_y_continuous(labels = scales::comma)  # Format y-axis numbers with commas
+  scale_y_continuous(labels = scales::comma)
 
 # Save the plot as a PNG file
 ggsave("BenchmarkDotNet.Artifacts/results/FibonacciBenchmark-plot.png", plot = p, width = 10, height = 6, dpi = 300)
