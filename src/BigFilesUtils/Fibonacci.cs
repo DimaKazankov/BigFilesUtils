1d6fd905ae0251f41a2678054ac5100c01a80677
Dim.Ka <dima.kazankov@gmail.com>
2024-09-27 05:39:48 +0200

    Added a simple beanchmark code

---
+++ src/BigFilesUtils/Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Exporters.Csv;
+
+namespace BigFilesUtils;
+
+[InProcess]
+[MemoryDiagnoser]
+[RankColumn, MinColumn, MaxColumn, Q1Column, Q3Column, AllStatisticsColumn]
+[MarkdownExporterAttribute.GitHub]
+[GcServer(true)]
+public class Fibonacci
+{
+    public IEnumerable<int> GetFibonacci(int count)
+    {
+        var w = 1;
+        var x = 1;
+
+        yield return x;
+        foreach (var _ in Enumerable.Range(1, count - 1))
+        {
+            var y = w + x;
+            yield return y;
+            w = x;
+            x = y;
+        }
+    }
+}
--- src/BigFilesUtils/Program.cs
+++ src/BigFilesUtils/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BigFilesUtils;
+
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
+
+[InProcess]
+[MemoryDiagnoser]
+[RankColumn, MinColumn, MaxColumn, Q1Column, Q3Column, AllStatisticsColumn]
+[MarkdownExporterAttribute.GitHub]
+[GcServer(true)]
+public class FibonacciBenchmark
+{
+    [Params(1, 2, 3, 5, 8, 13, 21, 34)]
+    public int Count { get; set; }
+
+    [Benchmark]
+    public void Fibonacci()
+    {
+        var xs = new Fibonacci().GetFibonacci(Count).ToList();
+    }
+}
