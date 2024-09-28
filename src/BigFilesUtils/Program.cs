b94125ad31af64dff4f0218a4ca5555abf22d259
Dim.Ka <dima.kazankov@gmail.com>
2024-09-28 11:18:16 +0200

    Playing with the most useful data vizualization

--- src/BigFilesUtils/Program.cs
+++ src/BigFilesUtils/Program.cs
@@ -32,8 +32,8 @@ public class CustomConfig : ManualConfig
     {
         // Set the time and size units
         SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default
-            .WithTimeUnit(null)      // Use seconds for time measurements
-            .WithSizeUnit(null)          // Use MB for size measurements
+            .WithTimeUnit(TimeUnit.Second)      // Use seconds for time measurements
+            .WithSizeUnit(SizeUnit.GB)          // Use MB for size measurements
             .WithMaxParameterColumnWidth(50);   // Adjust column width if necessary
     }
 }
@@ -57,7 +57,7 @@ public class FileGeneratorBenchmark
         new(1024 * 1024),
         new(100 * 1024L * 1024L),
         new(1024L * 1024L * 1024L),
-        // new(10 * 1024L * 1024L * 1024L),
+        new(10 * 1024L * 1024L * 1024L),
         // new(100 * 1024 * 1024L * 1024L)
     ];
 
